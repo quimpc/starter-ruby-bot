b3e7a15b6ef210036f84224a818a60b56b695f2d
quimpc <quim2003pc@gmail.com>
2016-03-19 15:34:49 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -59,7 +59,9 @@ client.on :message do |data|
     
   when 'Wie wird das Wetter?' ,  'Wie ist das Wetter?' then
     wetterinfo = Net::HTTP.get('api.openweathermap.org', '/data/2.5/weather?q=Bonn&appid=b1b15e88fa797225412429c1c50c122a')
-    client.message channel: data['channel'], text: wetterinfo
+    wetterinfo = JSON.parse wetterinfo
+    client.message channel: data['channel'], text: wetterinfo['weather']['description']
+    
    
   when /^bot/ then
     client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, I don\'t understand. \n#{help}"
